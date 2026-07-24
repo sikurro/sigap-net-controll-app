2194ab9894e87e7dbe7c7b4ab9f1a64bec32880f
Taufik Urrohman <sikurro@gmail.com>
2026-07-24 09:53:36 +0800

    feat: add visual feedback loading spinner to simulation calculation button (closes #97)

--- resources/js/Pages/Simulations/Index.vue
+++ resources/js/Pages/Simulations/Index.vue
@@ -609,8 +609,14 @@ const saveSimulation = () => {
                         </div>
 
                         <div class="mt-6 flex items-center justify-end">
-                            <PrimaryButton @click="calculateSimulation" :class="{ 'opacity-25': isCalculating }" :disabled="isCalculating">
-                                Jalankan Kalkulasi Simulasi
+                            <PrimaryButton @click="calculateSimulation" :class="{ 'opacity-75 cursor-wait': isCalculating }" :disabled="isCalculating">
+                                <div class="flex items-center justify-center space-x-2">
+                                    <svg v-if="isCalculating" class="animate-spin h-4 w-4 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+                                        <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
+                                        <path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+                                    </svg>
+                                    <span>{{ isCalculating ? 'Sedang Mengkalkulasi...' : 'Jalankan Kalkulasi Simulasi' }}</span>
+                                </div>
                             </PrimaryButton>
                         </div>
                         </div>
